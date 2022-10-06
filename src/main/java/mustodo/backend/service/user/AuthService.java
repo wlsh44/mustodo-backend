72215ae781e12dd325483f9a26aec849d7db05cb
wlsh44 <k87913j@gmail.com>
2022-10-06 18:07:56 +0900

    feat: authService  #1

---
+++ src/main/java/mustodo/backend/service/user/AuthService.java
@@ -0,0 +1,13 @@
+package mustodo.backend.service.user;
+
+import lombok.RequiredArgsConstructor;
+import mustodo.backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+
+}
