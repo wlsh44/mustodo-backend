5b2ddac8409c76b2274d7035041f65fd8a94de22
wlsh44 <k87913j@gmail.com>
2022-10-07 02:29:47 +0900

    test: 이메일 인증 테스트 추가 #1

--- src/test/java/mustodo/backend/service/user/mail/EmailAuthSenderUnitTest.java
+++ src/test/java/mustodo/backend/service/user/mail/EmailAuthSenderUnitTest.java
@@ -1,11 +1,9 @@
 package mustodo.backend.service.user.mail;
 
 import mustodo.backend.config.EmailConfig;
-import mustodo.backend.dto.MessageDto;
 import mustodo.backend.dto.user.EmailAuthDto;
 import mustodo.backend.entity.User;
 import mustodo.backend.entity.embedded.EmailAuth;
-import mustodo.backend.exception.UserException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,17 +15,14 @@ import org.springframework.mail.javamail.JavaMailSender;
 import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
 
-import static mustodo.backend.enums.error.SignUpErrorCode.INVALID_EMAIL_AUTH_KEY;
-import static mustodo.backend.enums.response.UserResponseMsg.EMAIL_AUTH_SUCCESS;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
-class EmailAuthServiceUnitTest {
+class EmailAuthSenderUnitTest {
 
     @InjectMocks
-    EmailAuthService emailAuthService;
+    EmailAuthSender emailAuthSender;
 
     @Mock
     EmailConfig emailConfig;
@@ -42,40 +37,7 @@ class EmailAuthServiceUnitTest {
 
     User user;
 
-    @Test
-    @DisplayName("유저 인증 성공")
-    void authUserSuccessTest() {
-        //given
-        user = User.builder()
-                .emailAuth(new EmailAuth("123456", false))
-                .build();
-        dto = EmailAuthDto.builder()
-                .authKey("123456")
-                .build();
 
-        //when
-        MessageDto messageDto = emailAuthService.authUser(user, dto);
-
-        //then
-        assertThat(messageDto.getMessage()).isEqualTo(EMAIL_AUTH_SUCCESS);
-    }
-
-    @Test
-    @DisplayName("유저 인증 실패")
-    void authUserFailTest() {
-        //given
-        user = User.builder()
-                .emailAuth(new EmailAuth("123456", false))
-                .build();
-        dto = EmailAuthDto.builder()
-                .authKey("456789")
-                .build();
-
-        //when
-        assertThatThrownBy(() -> emailAuthService.authUser(user, dto))
-                .isInstanceOf(UserException.class)
-                .hasMessage(INVALID_EMAIL_AUTH_KEY.getErrMsg());
-    }
 
     @Test
     @DisplayName("인증 메일 전송 성공")
@@ -97,7 +59,7 @@ class EmailAuthServiceUnitTest {
                 .willReturn("admin@admin.com");
 
         //when
-        String emailKey = emailAuthService.sendAuthMail(user);
+        String emailKey = emailAuthSender.sendAuthMail(user);
 
         //then
         assertThat(emailKey).isEqualTo(expectedEmailKey);
