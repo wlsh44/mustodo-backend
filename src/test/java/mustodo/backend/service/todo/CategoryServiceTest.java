a15924f00b6bcd1563f24271690ed40267094ec2
wlsh44 <k87913j@gmail.com>
2022-10-15 23:58:04 +0900

    test: 카테고리 생성 service 테스트 코드 추가 #17

---
+++ src/test/java/mustodo/backend/service/todo/CategoryServiceTest.java
@@ -0,0 +1,66 @@
+package mustodo.backend.service.todo;
+
+import mustodo.backend.dto.MessageDto;
+import mustodo.backend.dto.category.NewCategoryDto;
+import mustodo.backend.entity.User;
+import mustodo.backend.repository.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static mustodo.backend.enums.response.CategoryResponseMsg.CREATE_CATEGORY_SUCCESS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceTest {
+
+    @InjectMocks
+    CategoryService categoryService;
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+    User user;
+
+    @BeforeEach
+    void init() {
+        user = User.builder()
+                .build();
+    }
+
+    @Nested
+    @DisplayName("카테고리 생성 태스트")
+    class SaveTest {
+
+        NewCategoryDto dto;
+
+        @BeforeEach
+        void init() {
+            dto = NewCategoryDto.builder()
+                    .name("test")
+                    .color("FFFFFF")
+                    .build();
+        }
+
+        @Test
+        @DisplayName("카테고리 생성 성공")
+        void saveSuccessTest() {
+            //given
+            MessageDto messageDto = MessageDto.builder()
+                    .message(CREATE_CATEGORY_SUCCESS)
+                    .build();
+
+            //when
+            MessageDto res = categoryService.save(user, dto);
+
+            //then
+            assertThat(res).isEqualTo(messageDto);
+        }
+    }
+}
