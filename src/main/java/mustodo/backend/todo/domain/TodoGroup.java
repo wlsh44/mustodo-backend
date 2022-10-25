547a30dd2143ace6a873bf29be359dff776af759
wlsh44 <k87913j@gmail.com>
2022-10-26 01:05:22 +0900

    feat: todo 엔티티 추가 #33

---
+++ src/main/java/mustodo/backend/todo/domain/TodoGroup.java
@@ -0,0 +1,31 @@
+package mustodo.backend.todo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoGroup {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate dueDate;
+}
