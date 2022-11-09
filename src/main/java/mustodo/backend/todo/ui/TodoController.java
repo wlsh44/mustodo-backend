693bb83ac226ac6eb447168f06dabba018735fb9
wlsh44 <k87913j@gmail.com>
2022-11-09 15:09:42 +0900

    feat: 할 일 체크 controller 추가 #46

--- src/main/java/mustodo/backend/todo/ui/TodoController.java
+++ src/main/java/mustodo/backend/todo/ui/TodoController.java
@@ -11,6 +11,7 @@ import mustodo.backend.todo.ui.dto.NewTodoDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -55,5 +56,10 @@ public class TodoController {
     @DeleteMapping("/{todoId}")
     public void deleteTodo(@Login User user, @PathVariable Long todoId) {
         todoService.deleteTodo(user, todoId);
+    }
+
+    @PatchMapping("/{todoId}")
+    public void checkAchieve(@Login User user, @PathVariable Long todoId) {
+        todoService.checkAchieve(user, todoId);
     }
 }
