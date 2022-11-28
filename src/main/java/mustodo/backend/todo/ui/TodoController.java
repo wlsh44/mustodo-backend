9c894cf7aeaa0946a1dee6eacc5635194a5ab19e
wlsh44 <k87913j@gmail.com>
2022-11-28 17:55:45 +0900

    feat: 할 일 조회 controller 추가 #36

--- src/main/java/mustodo/backend/todo/ui/TodoController.java
+++ src/main/java/mustodo/backend/todo/ui/TodoController.java
@@ -2,6 +2,7 @@ package mustodo.backend.todo.ui;
 
 import lombok.RequiredArgsConstructor;
 import mustodo.backend.todo.ui.dto.TodoByDateResponse;
+import mustodo.backend.todo.ui.dto.TodoDetailResponse;
 import mustodo.backend.todo.ui.dto.TodoResponse;
 import mustodo.backend.user.domain.User;
 import mustodo.backend.auth.ui.resolver.Login;
@@ -61,5 +62,12 @@ public class TodoController {
     @PatchMapping("/{todoId}")
     public void checkAchieve(@Login User user, @PathVariable Long todoId) {
         todoService.checkAchieve(user, todoId);
+    }
+
+    @GetMapping("/{todoId}")
+    public ResponseEntity<TodoDetailResponse> findById(@Login User user, @PathVariable Long todoId) {
+        TodoDetailResponse todoDetailResponse = todoService.findById(user, todoId);
+
+        return ResponseEntity.ok(todoDetailResponse);
     }
 }
