65ad347015b9b3a3d5862686b83a3a4b15bd8934
wlsh44 <k87913j@gmail.com>
2022-12-09 20:23:04 +0900

    fix: 이미지 저장 경로 수정 #64

--- src/main/java/mustodo/backend/config/ImageConfig.java
+++ src/main/java/mustodo/backend/config/ImageConfig.java
@@ -7,8 +7,16 @@ import org.springframework.boot.context.properties.ConstructorBinding;
 
 @Getter
 @ConstructorBinding
-@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "images.profile")
 @ConfigurationProperties(prefix = "images.path")
 public class ImageConfig {
-    private final String profile;
+    private final String rootPath;
+    private final String path;
+    private final String defaultImage;
+
+    public ImageConfig(String rootPath, String path, String defaultImage) {
+        this.rootPath = System.getProperty("user.dir") + rootPath;
+        this.path = path;
+        this.defaultImage = defaultImage;
+    }
 }
