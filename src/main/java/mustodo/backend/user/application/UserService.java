cd44d54cfe6ab62715ffa016b323351a462f61cc
wlsh44 <k87913j@gmail.com>
2022-12-09 20:17:12 +0900

    feat: 프로필 추가 컨트롤러 추가 #64

--- src/main/java/mustodo/backend/user/application/UserService.java
+++ src/main/java/mustodo/backend/user/application/UserService.java
@@ -7,14 +7,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityManager;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final ImageConfig imageConfig;
+    private final EntityManager entityManager;
 
     @Transactional
     public void updateProfileImage(User user, MultipartFile multipartFile) {
+        entityManager.merge(user);
         user.updateProfileImage(multipartFile, imageConfig.getProfile());
     }
 }
