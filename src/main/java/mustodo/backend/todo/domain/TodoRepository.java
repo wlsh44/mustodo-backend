d47c8691d1aa96cc26d32250ab4b105d9ce25773
wlsh44 <k87913j@gmail.com>
2022-12-13 17:55:00 +0900

    fix: 날짜 별 할 일 api 수정

--- src/main/java/mustodo/backend/todo/domain/TodoRepository.java
+++ src/main/java/mustodo/backend/todo/domain/TodoRepository.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 public interface TodoRepository extends JpaRepository<Todo, Long> {
     List<Todo> findAllByCategory_Id(Long categoryId);
 
-    @Query("select todo from Todo todo where (todo.user=:user and todo.date=:date) or (todo.dDay=true)")
+    @Query("select todo from Todo todo where todo.user=:user and ((todo.date=:date) or (todo.dDay=true and :date <= todo.date and todo.achieve = false))")
     List<Todo> findAllByUserAndDate(User user, LocalDate date);
     Optional<Todo> findByUserAndId(User user, Long todoId);
 
