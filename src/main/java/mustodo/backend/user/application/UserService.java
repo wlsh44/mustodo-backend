e52c409ac879c6064b5ef77da6443d328f7ae01c
wlsh44 <k87913j@gmail.com>
2022-12-15 16:46:17 +0900

    feat: 마이페이지 데이터 조회 기능 추가 #77

--- src/main/java/mustodo/backend/user/application/UserService.java
+++ src/main/java/mustodo/backend/user/application/UserService.java
@@ -2,7 +2,10 @@ package mustodo.backend.user.application;
 
 import lombok.RequiredArgsConstructor;
 import mustodo.backend.config.ImageConfig;
+import mustodo.backend.exception.user.UserNotFoundException;
 import mustodo.backend.user.domain.User;
+import mustodo.backend.user.domain.UserRepository;
+import mustodo.backend.user.ui.dto.MeResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,10 +18,17 @@ public class UserService {
 
     private final ImageConfig imageConfig;
     private final EntityManager entityManager;
+    private final UserRepository userRepository;
 
     @Transactional
     public void updateProfileImage(User user, MultipartFile multipartFile) {
         entityManager.merge(user);
         user.updateProfileImage(multipartFile, imageConfig);
+    }
+
+    @Transactional(readOnly = true)
+    public MeResponse getMe(User user) {
+        return userRepository.findFollowCountByUser(user)
+                .orElseThrow(() -> new UserNotFoundException(user.getEmail()));
     }
 }
---
+++ src/main/java/mustodo/backend/user/ui/dto/MeResponse.java
@@ -0,0 +1,6 @@
+package mustodo.backend.user.ui.dto;
+
+public interface MeResponse {
+    int getFollowingCount();
+    int getFollowerCount();
+}
